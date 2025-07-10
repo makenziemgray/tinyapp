360dd41304f6938d0c6226bb1e48d18039443fac
makenziemgray <makenziemgray0102@gmail.com>
2025-07-10 10:05:02 -0400

    refactored login route

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
 
 const app = express();
-const PORT = 8080;
+const PORT = 8000;
 
 const {
   generateRandomString,
@@ -14,16 +14,13 @@ const {
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-// override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 app.use(cookieSession({
   name: 'session',
   keys: ["hXlF4ON92Ej4kH_nLmzkqI_Zki0"],
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  maxAge: 24 * 60 * 60 * 1000
 }));
 
-/*-------------------------  DATABASES  -------------------------*/
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -56,139 +53,81 @@ const users = {
   },
 };
 
-/*-----------------------  RENDER ROUTES  -----------------------*/
 app.get("/", (req, res) => {
-  // set userID to session cookie
   const userID = req.session.userID;
-  // user is not logged in, redirect to login page
   if (!users[userID]) {
-    res.redirect("/login");
-    return;
+    return res.redirect("/login");
   }
-  // user is logged in, redirect to urls page
   res.redirect("/urls");
 });
 
-// View users URLs
 app.get("/urls", (req, res) => {
-  // set userID to session cookie
   const userID = req.session.userID;
-
-  // user is not logged in, render error page
   if (!users[userID]) {
-    res.status(403);
-    res.render("errorPage", {
+    return res.status(403).render("errorPage", {
       status: 403,
       message: "Please Register or Login to view URLs",
       redirectLink: "/login"
     });
-    return;
   }
-
-  // filtered url database
   const database = urlsForUser(userID, urlDatabase);
   const templateVars = {
     urls: database,
     user: users[userID]
   };
-  // render urls_index page to user with template variables
   res.render("urls_index", templateVars);
 });
 
-// View route to create new URL
 app.get("/urls/new", (req, res) => {
-  // set userID to session cookie
   const userID = req.session.userID;
-
-  // user is not logged in, redirect to login page
   if (!users[userID]) {
-    res.redirect("/login");
-    return;
+    return res.redirect("/login");
   }
-
-  const templateVars = {
-    user: users[userID]
-  };
-  // render urls_new page to user with template variables
+  const templateVars = { user: users[userID] };
   res.render("urls_new", templateVars);
 });
 
-// View route to registration page
 app.get("/register", (req, res) => {
-  // set userID to session cookie
   const userID = req.session.userID;
-
-  // user is logged in, redirect
   if (users[userID]) {
-    res.redirect("/urls");
-    return;
+    return res.redirect("/urls");
   }
-
-  const templateVars = {
-    user: users[userID]
-  };
-  // render register page with template variables
-  res.render("register", templateVars);
+  res.render("register", { user: null });
 });
 
-// View route to login page
 app.get("/login", (req, res) => {
-  // set userID to session cookie
   const userID = req.session.userID;
-
-  // user is logged in, redirect
   if (users[userID]) {
-    res.redirect("/urls");
-    return;
+    return res.redirect("/urls");
   }
-
-  const templateVars = {
-    user: users[userID]
-  };
-  // render login page with template variables
-  res.render("login", templateVars);
+  res.render("login", { user: null });
 });
 
-// View route to one URL
 app.get("/urls/:id", (req, res) => {
-  // set id to parameter from GET request
   const id = req.params.id;
-  // set userID to session cookie
   const userID = req.session.userID;
 
-  // url does not exist in database, render error page
   if (!urlDatabase[id]) {
-    res.status(404);
-    res.render("errorPage", {
+    return res.status(404).render("errorPage", {
       status: 404,
       message: `${id} URL does not exist`,
       redirectLink: null
     });
-    return;
   }
-
-  // user is not logged in, render error page
   if (!users[userID]) {
-    res.status(403);
-    res.render("errorPage", {
+    return res.status(403).render("errorPage", {
       status: 403,
       message: "Please Register or Login to view URLs",
       redirectLink: "/login"
     });
-    return;
   }
-
-  // user does not own url, render error page
   if (urlDatabase[id].userID !== userID) {
-    res.status(403);
-    res.render("errorPage", {
+    return res.status(403).render("errorPage", {
       status: 403,
       message: "You do not have access to this URL.",
       redirectLink: null
     });
-    return;
   }
-
   const templateVars = {
     id,
     user: users[userID],
@@ -198,256 +137,151 @@ app.get("/urls/:id", (req, res) => {
     uniqueVisits: urlDatabase[id].uniqueVisits,
     visitors: urlDatabase[id].visitors
   };
-  // render urls_show page with template variables
   res.render("urls_show", templateVars);
 });
 
-// Route to redirect to long URL
-app.get("/u/:id", (req, res) => {
-  // set id to parameter from GET request
-  const id = req.params.id;
-  // set longURL to value of longURL from parameter passed in GET request
-  const longURL = urlDatabase[req.params.id].longURL;
 
-  // URL VISIT TRACKING
-  // set session views to value or if none, 0, and then add one
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id]?.longURL;
+  if (!longURL) {
+    return res.status(404).send("URL not found");
+  }
+
   req.session.views = (req.session.views || 0) + 1;
-  // update url visit count
   urlDatabase[id].visitCount = req.session.views;
 
-
-  // if session cookie exists, set it to visitor id, if not then random 6 character string
   const visitorID = req.session.visitorID || generateRandomString();
-  // set cookie to visitorID (if new = random string, if exists = visitorID)
   req.session.visitorID = visitorID;
-  // check if visitID exists in visitors already
+
   if (!urlDatabase[id].visitors[visitorID]) {
-    // new visitor, so add visitor information to visitors object of tiny URL
     urlDatabase[id].visitors[visitorID] = {
-      // add timestamp of unique visitor
       timestamp: new Date().toUTCString(),
     };
-    // if no value, set to zero, and each time increase unique visits by 1
     urlDatabase[id].uniqueVisits = (urlDatabase[id].uniqueVisits || 0) + 1;
   }
 
-  // redirect user to page of the longURL
   res.redirect(longURL);
 });
 
-/*------------------------  API ROUTES  ------------------------*/
-// Create new URL
 app.post("/urls", (req, res) => {
-  // set userID to session cookie
   const userID = req.session.userID;
-
-  // user is not logged in, render error page
   if (!users[userID]) {
-    res.status(403);
-    res.render("errorPage", {
+    return res.status(403).render("errorPage", {
       status: 403,
       message: "Please Register or Login to view URLs",
       redirectLink: "/login"
     });
-    return;
   }
-
-  // set longURL to POST request value
-  const longURL = req.body.longURL;
-  // set id to random 6 character string
   const id = generateRandomString();
-  // add url object to urlDatabase and set values
   urlDatabase[id] = {
-    longURL: longURL,
-    userID: userID,
-    // set date short URL was created
+    longURL: req.body.longURL,
+    userID,
     createdDate: new Date().toUTCString(),
-    // starting view count
     visitCount: 0,
     uniqueVisits: 0,
     visitors: {}
   };
-  // redirect user to url page
   res.redirect(`/urls/${id}`);
 });
 
-// Update URL
 app.put("/urls/:id", (req, res) => {
-  // set id to parameter from GET request
   const id = req.params.id;
-  // set longURL to POST request value
-  const longURL = req.body.longURL;
-  // set userID to session cookie
   const userID = req.session.userID;
 
-  // id does not exist, render error page
   if (!urlDatabase[id]) {
-    res.status(404);
-    res.render("errorPage", {
+    return res.status(404).render("errorPage", {
       status: 404,
       message: `${id} URL does not exist`,
       redirectLink: null
     });
-    return;
   }
-
-  // user is not logged in, render error page
-  if (!users[userID]) {
-    res.status(403);
-    res.render("errorPage", {
-      status: 403,
-      message: "Please Register or Login to view URLs",
-      redirectLink: "/login"
-    });
-    return;
-  }
-
-  // user does not own url, render error page
-  if (urlDatabase[id].userID !== userID) {
-    res.status(403);
-    res.render("errorPage", {
+  if (!users[userID] || urlDatabase[id].userID !== userID) {
+    return res.status(403).render("errorPage", {
       status: 403,
       message: "You do not have access to this URL.",
-      redirectLink: null
+      redirectLink: "/login"
     });
-    return;
   }
-
-  // update long url to url entered in POST request
-  urlDatabase[id].longURL = longURL;
-  // redirect user to urls page
+  urlDatabase[id].longURL = req.body.longURL;
   res.redirect("/urls");
 });
 
-// Delete URL
 app.delete("/urls/:id", (req, res) => {
-  // set id to value from POST request
   const id = req.params.id;
-  // set userID to session cookie
   const userID = req.session.userID;
 
-  // id does not exist in url database, render error page
   if (!urlDatabase[id]) {
-    res.status(404);
-    res.render("errorPage", {
+    return res.status(404).render("errorPage", {
       status: 404,
       message: `${id} URL does not exist`,
       redirectLink: null
     });
-    return;
   }
-
-  // user is not logged in, render error page
-  if (!users[userID]) {
-    res.status(403);
-    res.render("errorPage", {
-      status: 403,
-      message: "Please Register or Login to view URLs",
-      redirectLink: "/login"
-    });
-    return;
-  }
-
-  // user does not own url, render error page
-  if (urlDatabase[id].userID !== userID) {
-    res.status(403);
-    res.render("errorPage", {
+  if (!users[userID] || urlDatabase[id].userID !== userID) {
+    return res.status(403).render("errorPage", {
       status: 403,
       message: "You do not have access to this URL.",
-      redirectLink: null
+      redirectLink: "/login"
     });
-    return;
   }
-
-  // clear url id from url database, which deletes the url
   delete urlDatabase[id];
-  // redirect user to urls page, url from POST request will be gone
   res.redirect("/urls");
 });
 
-// Register
 app.post("/register", (req, res) => {
-  // set id to random 6 character string
   const id = generateRandomString();
-  // set email and password to values from POST request
-  const email = req.body.email;
-  const password = req.body.password;
-  // hash password using bcrypt
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const { email, password } = req.body;
 
-  // no email or password entered, render error page
   if (!email || !password) {
-    res.status(400);
-    res.render("errorPage", {
+    return res.status(400).render("errorPage", {
       status: 400,
       message: "Please enter valid Email Address and Password",
       redirectLink: "/register"
     });
-    return;
   }
-
-  // email already exists in users database, can't register with duplicate email
   if (getUserByEmail(email, users)) {
-    res.status(400);
-    res.render("errorPage", {
+    return res.status(400).render("errorPage", {
       status: 400,
       message: "Account already exists with this email",
       redirectLink: "/register"
     });
-    return;
   }
-
-  // add user object to users database and set values
+  const hashedPassword = bcrypt.hashSync(password, 10);
   users[id] = { id, email, hashedPassword };
-  // set cookie for user using their id
   req.session.userID = id;
-  // redirect user to urls page
   res.redirect("/urls");
 });
 
-// Login
 app.post("/login", (req, res) => {
-  // set email and password to values from POST request
-  const email = req.body.email;
-  const password = req.body.password;
-  // find user in users database
+  const { email, password } = req.body;
   const user = getUserByEmail(email, users);
 
-  // user is not in database, render error page
-  if (!user) {
-    res.status(404);
-    res.render("errorPage", {
-      status: 404,
-      message: "Email cannot be found. Please Register for your account first.",
-      redirectLink: "/register"
-    });
-    return;
-  }
-  // entered password does not match password in database, render error page
-  if (!bcrypt.compareSync(password, user.hashedPassword)) {
-    res.status(401);
-    res.render("errorPage", {
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword)) {
+    return res.status(401).render("errorPage", {
       status: 401,
-      message: "Incorrect Password. Try again.",
+      message: "Invalid email or password.",
       redirectLink: "/login"
     });
-    return;
   }
-  // correct login information, set cookie for user using their id
   req.session.userID = user.id;
-  // redirect user to urls page
   res.redirect("/urls");
 });
 
-// Logout
 app.post("/logout", (req, res) => {
-  // clear cookie
   req.session = null;
-  // redirect user to login page
   res.redirect("/login");
 });
 
-/*-----------------------  LISTEN METHOD  -----------------------*/
+// Catch-all 404 route
+app.use((req, res) => {
+  res.status(404).render("errorPage", {
+    status: 404,
+    message: "Page Not Found",
+    redirectLink: "/login"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
